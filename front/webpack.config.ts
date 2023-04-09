77978aa029d52d442eb0668083a0a693ef980543
yiminwook <grs0416@gmail.com>
2023-04-09 14:48:34 +0900

    Add: webpack webpack-dev-server config

---
+++ front/webpack.config.ts
@@ -0,0 +1,91 @@
+import { resolve, join } from 'path';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import {
+  Configuration as WebpackConfiguration,
+  EnvironmentPlugin,
+  HotModuleReplacementPlugin,
+  LoaderOptionsPlugin,
+} from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+const config: Configuration = {
+  name: 'sleact_font',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'hidden-source-map' : 'inline-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      '@hooks': resolve(__dirname, 'hooks'),
+      '@components': resolve(__dirname, 'components'),
+      '@layouts': resolve(__dirname, 'layouts'),
+      '@pages': resolve(__dirname, 'pages'),
+      '@utils': resolve(__dirname, 'utils'),
+      '@typings': resolve(__dirname, 'typings'),
+    },
+  },
+  entry: {
+    app: './client.tsx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/i,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env', //브라우저 지원
+              {
+                targets: { browsers: ['last 2 chrome versions'] },
+                debug: isDevelopment,
+              },
+            ],
+            '@babel/preset-react',
+            '@babel/preset-typescript',
+          ],
+        },
+        exclude: join(__dirname, 'node_modules'),
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({ async: false }),
+    new EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
+  ],
+  output: {
+    path: join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/dist/',
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3090,
+    devMiddleware: { publicPath: '/dist/' },
+    static: { directory: resolve(__dirname) },
+  },
+};
+
+if (isDevelopment && config.plugins) {
+  config.plugins.push(new HotModuleReplacementPlugin());
+  config.plugins.push(new ReactRefreshWebpackPlugin());
+  config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: false }));
+}
+
+if (!isDevelopment && config.plugins) {
+  config.plugins.push(new LoaderOptionsPlugin({ minimize: true }));
+  config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
+}
+
+export default config;
