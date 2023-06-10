11bebaef40dfdcfa766ecd0a3e9383ff19f6ae6e
yiminwook <grs0416@gmail.com>
2023-06-11 04:08:34 +0900

    Add: SWR debug tool

--- front/client.tsx
+++ front/client.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import SWRDevTools from '@jjordy/swr-devtools';
 
 import App from '@layouts/App';
 
 const root = createRoot(document.getElementById('app') as HTMLElement);
 
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-);
+const app =
+  process.env.NODE_ENV === 'development' ? (
+    <SWRDevTools>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SWRDevTools>
+  ) : (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+
+root.render(app);
