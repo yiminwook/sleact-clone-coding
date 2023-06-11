96bc3e2af59b3068416cdc6f2c1b47eef1d74852
yiminwook <grs0416@gmail.com>
2023-06-11 22:11:49 +0900

    Add: setting.md

--- front/client.tsx
+++ front/client.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import SWRDevTools from '@jjordy/swr-devtools';
-
 import App from '@layouts/App';
+import axios from 'axios';
 
 const root = createRoot(document.getElementById('app') as HTMLElement);
+const isDevelopment = process.env.NODE_ENV === 'development';
 
-const app =
-  process.env.NODE_ENV === 'development' ? (
-    <SWRDevTools>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </SWRDevTools>
-  ) : (
+const app = isDevelopment ? (
+  <SWRDevTools>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  );
+  </SWRDevTools>
+) : (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 root.render(app);
